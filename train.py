948cc6aa1b3549bd0f2e62c101d6b61e7fe28a71
elwhyjay <lyj4032@naver.com>
2024-09-24 00:17:36 +0000

    feat: mult task learning 구현

--- train.py
+++ train.py
@@ -6,7 +6,7 @@ import pytorch_lightning as pl
 from pytorch_lightning.callbacks import EarlyStopping
 from pytorch_lightning.callbacks import ModelCheckpoint
 from pytorch_lightning.loggers import WandbLogger
-from module.model import Model
+from module.model import Model,MultitaksModel
 from module.dataset import Dataloader
 import wandb
 # seed 고정
@@ -22,47 +22,58 @@ if __name__ == '__main__':
     # 터미널 실행 예시 : python3 run.py --batch_size=64 ...
     # 실행 시 '--batch_size=64' 같은 인자를 입력하지 않으면 default 값이 기본으로 실행됩니다
     parser = argparse.ArgumentParser()
-    parser.add_argument('--model_name', default='klue/roberta-large', type=str)
-    parser.add_argument('--batch_size', default=16, type=int)
-    parser.add_argument('--max_epoch', default=20, type=int)
+    parser.add_argument('--model_name', default='klue/roberta-base', type=str)
+    parser.add_argument('--batch_size', default=128, type=int)
+    parser.add_argument('--max_epoch', default=10, type=int)
     parser.add_argument('--shuffle', default=True)
-    parser.add_argument('--learning_rate', default=5e-6, type=float)
+    parser.add_argument('--learning_rate', default=1e-5, type=float)
     parser.add_argument('--train_path', default='./data/train_preprocessed_augmented.csv')
-    parser.add_argument('--loss_function', default='L1')
+    parser.add_argument('--loss_function', default='MSE')
     parser.add_argument('--dev_path', default='./data/val_preprocessed.csv')
     parser.add_argument('--test_path', default='./data/val_preprocessed.csv')
     parser.add_argument('--predict_path', default='../test.csv')
+    parser.add_argument('--multi_task', default=True)
     args = parser.parse_args(args=[])
 
     save_path = f"save_model/{args.model_name.replace('/','-')}_Batch-size:{args.batch_size}_Max-epochs:{args.max_epoch}/"
 
     early_stopping_callbacks = EarlyStopping(
         monitor = 'val_pearson',
-        patience= 10,
+        patience= 7,
         mode = 'max'
     )
     model_checkpoint_callbacks = ModelCheckpoint(
         monitor = 'val_pearson',
-        dirpath=save_path, ## 수정필요
+        dirpath=save_path, 
         save_top_k=1,
         mode = 'max',
         filename="first_model"
     )
 
-    ##작성필요
+    
     wandblogger = WandbLogger(project="STSporj",
                               name = f"{args.model_name.replace('/','-')}//loss_func:{args.loss_function}//batch_size:{args.batch_size}")
 
     # dataloader와 model을 생성합니다.
     dataloader = Dataloader(args.model_name, args.batch_size, args.shuffle, args.train_path, args.dev_path,
-                            args.test_path, args.predict_path)
-    model = Model(args.model_name, args.learning_rate,args.loss_function)
+                            args.test_path, args.predict_path, args.multi_task)
     
     #<person> token 추가.
     person_token = ['<person>']
     dataloader.token_add(person_token)
+
+    if args.multi_task:
+        weight = 0.7
+        model = MultitaksModel(args.model_name, args.learning_rate,args.loss_function,weight)
+        model.encoder.resize_token_embeddings(len(dataloader.tokenizer))
+    else:
+        model = Model(args.model_name, args.learning_rate,args.loss_function)
+        model.plm.resize_token_embeddings(len(dataloader.tokenizer))
     
-    model.plm.resize_token_embeddings(len(dataloader.tokenizer))
+    
+    
+    
+    
     
     
     # gpu가 없으면 accelerator="cpu"로 변경해주세요, gpu가 여러개면 'devices=4'처럼 사용하실 gpu의 개수를 입력해주세요
